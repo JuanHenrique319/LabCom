d3bcf41d4074c8ce86ee8920c3d8e071193f105a
JuanHenrique319 <80597368+JuanHenrique319@users.noreply.github.com>
2022-11-07 00:07:24 -0300

    Update 2
    ++ correções de script
    ++finalização sistema de fala
    ++finalização hud em game
    ++Sistema de bandeira
    ++Menus melhorados
    ++outros acertos

---
+++ Assets/Scripts/Sound/SoundPlaylist.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlaylist : MonoBehaviour
+{
+    public bool playing;
+    //Fonte de audio
+    public AudioSource source;
+    //vetor para as musicas serem indexadas
+    [Header("vetor de músicas a serem tocadas")]
+    public AudioClip[] musicclips;
+    void Start()
+    {
+        //defini que está tocando ao iniciar e inicia o enum que roda a lista de musicas
+        playing = true;
+        StartCoroutine(Playmusicloop());
+    }
+
+    // Update is called once per frame
+    IEnumerator Playmusicloop()
+    //play em loop de um vetor de clipes
+    {
+        yield return null;
+        while (playing)
+        {
+            for (int i = 0; i < musicclips.Length; i++)
+            {
+                source.clip = musicclips[i];
+                source.Play();
+                while (source.isPlaying)
+                {
+                    yield return null;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/botões/MenuEmGame.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using TMPro.EditorUtilities;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuEmGame : MonoBehaviour
+{
+   //ele salva os dados aqui setados que são chmados via clique por metodos e salva em playerprefs e resgata esses valores em Dadosdetexto
+    public TextMeshProUGUI txtFala;
+    public TextMeshProUGUI txtNome;
+    public GameObject Menu;
+    private bool IsActive=false;
+    //tamanho da fonte
+    public void fontPEQ()
+    {
+        //txtFala.fontSize = 18;
+        //txtNome.fontSize = 18;
+        PlayerPrefs.SetInt("Size", 18);
+        PlayerPrefs.Save();
+    }
+    public void fontMED()
+    {
+        //txtFala.fontSize = 22;
+        //txtNome.fontSize = 22;
+        PlayerPrefs.SetInt("Size", 22);
+        PlayerPrefs.Save();
+    }
+    public void fontGRA()
+    {
+        //txtFala.fontSize = 24;
+        //txtNome.fontSize = 24;
+        PlayerPrefs.SetInt("Size", 24);
+        PlayerPrefs.Save();
+    }
+    //cor da fonte
+    public void corBRA()
+    {
+        // txtFala.color = Color.white;
+        //txtNome.color = Color.white;
+        //txtFala.outlineWidth = 0;
+        //txtNome.outlineWidth = 0;
+        PlayerPrefs.SetString("Color", "white");
+        PlayerPrefs.SetFloat("Outline", 0f);
+        PlayerPrefs.Save();
+    }
+    public void corYEL()
+    {
+        //txtFala.color = Color.yellow;
+        //txtNome.color = Color.yellow;
+        //txtFala.outlineWidth = 0.2f;
+        //txtNome.outlineWidth = 0.2f;
+        PlayerPrefs.SetString("Color", "yellow");
+        PlayerPrefs.SetFloat("Outline", 0.2f);
+        PlayerPrefs.Save();
+    }
+    //ativa e desativa o menu 
+    public void MenuIn()
+    {
+        IsActive =! IsActive;
+        Menu.SetActive(IsActive);
+    }
+}
